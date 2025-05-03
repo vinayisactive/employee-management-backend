10c40a97a0cc1913506980ab3cf0a31425d50012
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-05-03 12:56:31 +0530

    feat: update to production settings (cookies)

--- src/controllers/auth-controllers.ts
+++ src/controllers/auth-controllers.ts
@@ -61,7 +61,7 @@ export const logIn = async (
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "lax",
       path: "/",
       maxAge: 24*60*60*1000
@@ -102,7 +102,7 @@ export const logOut = async(req: Request, res: Response) => {
     
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false, 
+      secure: true, 
       sameSite: "lax",
       path: "/"
     }); 
